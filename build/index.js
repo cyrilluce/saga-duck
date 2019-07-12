16849661e07ab5e7d13051bc3bb457205f588f12
cluezhang <cluezhang@tencent.com>
2019-07-12 15:02:55 +0800

    chore: remove build dir

--- build/index.js
+++
@@ -1,6 +0,0 @@
-export { default as BaseDuck } from "./BaseDuck";
-export { default as Duck } from "./Duck";
-export { default as ComposableDuck, default as DuckMap } from "./ComposableDuck";
-export { default as DuckRuntime, INIT, END } from "./DuckRuntime";
-export { purify, shouldComponentUpdate } from "./purify";
-export { asResult, reduceFromPayload, createToPayload, memorize } from "./helper";
--- build/purify.d.ts
+++
@@ -1,7 +0,0 @@
-import { StatelessComponent, ComponentClass } from "react";
-export declare function shouldComponentUpdate(instance: any, props: any, state: any): boolean;
-export interface PurifyType {
-    <T>(component: StatelessComponent<T>): ComponentClass<T>;
-    <T, C extends ComponentClass<T>>(component: C): C;
-}
-export declare const purify: PurifyType;
