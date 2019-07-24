4ea9db4b9abcf83f8883707d5ddf07aa74a4cd4d
cluezhang <cluezhang@tencent.com>
2019-07-24 21:23:49 +0800

    chore(deps): switch to npm lock

--- examples/build/examples/src/Counter.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import Duck from "./CounterDuck";
-import { DuckCmpProps } from "../../src/DuckRuntime";
-declare const _default: React.ComponentClass<DuckCmpProps<Duck>, any>;
-export default _default;
--- examples/build/examples/src/Root.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { DuckCmpProps } from '../../src/DuckRuntime';
-import Duck from './RootDuck';
-export default function Root({ duck, store, dispatch }: DuckCmpProps<Duck>): JSX.Element;
